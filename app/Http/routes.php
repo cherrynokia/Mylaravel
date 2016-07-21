884d272b58b3abbffd2e80bcac866d9d5d5c8651
cherrynokia <poat777@hotmail.com>
2016-07-21 14:49:58 +0800

    從資料庫顯示資料 並從網頁伺服器(view)顯示出來

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,12 +14,40 @@ use Illuminate\Http\Request;
 
 Route::get('/', function () {
 //    return view('welcome');
+    $dbTask = Task::orderBy("created_at", "asc")->get();//從資料庫撈出來的資料(陣列) 屬於ORM物件
+    
+    return view("tasks",["taskSets"=>$dbTask]);
 });
-Route::post('/task', function (Request $request) {
+
+Route::get('/task', function () {
+    return "網址列輸入ㄉ網址";
+});
+
+
+//接收表單,來增加新的任務  $request適從瀏覽器ㄉ表單來  $task->name 是資料庫的欄位 SAVE儲存在資料庫
+ //驗証 tasks.blade.php 傳來資料
+Route::post('/task', function (Request $request){
+    
+    $validator = Validator::make($request->all(),
+                                 ["name"=>"required|max:2"]
+                                 );
+     if($validator->fails())
+    {
+        return redirect("/")
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect("/");
+});
+
+//刪除任務
+Route::delete('/task/{task}', function (Task $task) {
     //
 });
-Route::delete('/task/{id}', function ($id) {
-    //
-});
+
 
 
